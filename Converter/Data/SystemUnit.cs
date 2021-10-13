210297da0d97d2928b7dc23935606c54263cf4a8
Filip Żabnicki <filip.zabnicki@globallogic.com>
2021-10-13 14:48:34 +0200

    Separete SystemAssiging, refactor SystemUnit.cs

--- Converter/Data/SystemUnit.cs
+++ Converter/Data/SystemUnit.cs
@@ -6,19 +6,12 @@ namespace Converter.Data
 {
     public class SystemUnit
     {
-        Dictionary<Unit, MeasureSystem> UnitSystem = new Dictionary<Unit, MeasureSystem>()
-        {
-            {Unit.Gram, MeasureSystem.SI },
-            {Unit.Miligram, MeasureSystem.SI },
-            {Unit.Pound, MeasureSystem.Imperial },
-            {Unit.Ounce, MeasureSystem.Imperial },
-        };
         internal Unit Unit { get; }
         internal MeasureSystem System { get; set; }
         public SystemUnit(Unit unit)
         {
             Unit = unit;
-            System = UnitSystem[unit];
+            System = UnitSystemDictionary.AssignSystem(unit);
         }
     }
 }
---
+++ Converter/Data/UnitSystemDictionary.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Converter.Data
+{
+    internal static class UnitSystemDictionary
+    {
+        static Dictionary<Unit, MeasureSystem> UnitSystem = new Dictionary<Unit, MeasureSystem>()
+        {
+                {Unit.Gram, MeasureSystem.SI },
+                {Unit.Miligram, MeasureSystem.SI },
+                {Unit.Pound, MeasureSystem.Imperial },
+                {Unit.Ounce, MeasureSystem.Imperial },
+        };
+        public static MeasureSystem AssignSystem(Unit unit)
+        {
+            return UnitSystem[unit];
+        }
+    }
+}
